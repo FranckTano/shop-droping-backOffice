da9cad842584df8a70fed486d1ed0901dd58c839
FranckTano <tanoguyfranck22@gmail.com>
2026-06-04 16:43:48 +0000

    Modif prod V2

--- backend/src/main/java/com/shopdropping/backoffice/config/WebConfig.java
+++ backend/src/main/java/com/shopdropping/backoffice/config/WebConfig.java
@@ -1,16 +1,11 @@
 package com.shopdropping.backoffice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${file.product.image-path:${user.home}/shop-dropping/products/images}")
-    private String imageUploadPath;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -20,12 +15,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String imagePath = imageUploadPath.endsWith("/") ? imageUploadPath : imageUploadPath + "/";
-        registry.addResourceHandler("/uploads/produits/**")
-                .addResourceLocations("file:" + imagePath);
     }
 }
