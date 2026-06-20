378e602d19342c60a8ea550ca839a38f04a0deb8
Franck Tano <tanoguyfranck22@gmail.com>
2026-06-20 13:14:43 +0000

    fix: nouvelle URL backend BO sur Render (shop-droping-backoffice)

--- frontend/environments/environment.prod.ts
+++ frontend/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://shop-droping-back-backend.onrender.com/api',
+    apiUrl: 'https://shop-droping-backoffice.onrender.com/api',
     frontOfficeUrl: 'https://momo-store.shop',
     url: 'https://shop-droping-back-office.vercel.app'
 };
